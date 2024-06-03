9e9b9c6afecff8bc215b0aa51140b54b170ac9c3
GKhiem <giakhiemvu19102002@gmail.com>
2024-06-03 17:23:23 +0700

    update sql

--- sql Script/createDb.sql
+++ sql Script/createDb.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE `web` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-use `web`;
+CREATE DATABASE `web`;
 
 CREATE TABLE `logininfo` (
   `loginId` int NOT NULL AUTO_INCREMENT,
@@ -34,7 +32,7 @@ CREATE TABLE `product` (
   `Pid` int NOT NULL AUTO_INCREMENT,
   `categoryId` int DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `status` varchar(50) DEFAULT "open",
+  `status` varchar(50) DEFAULT 'open',
   `price` decimal(10,2) DEFAULT NULL,
   `description` text,
   `recommend` tinyint(1) DEFAULT '0',
@@ -42,7 +40,7 @@ CREATE TABLE `product` (
   PRIMARY KEY (`Pid`),
   KEY `categoryId` (`categoryId`),
   CONSTRAINT `product_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `category` (`categoryId`)
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `image` (
   `imageID` int NOT NULL AUTO_INCREMENT,
   `Image` longblob,
@@ -55,11 +53,11 @@ CREATE TABLE `order` (
   `OrderID` int NOT NULL AUTO_INCREMENT,
   `Pid` int NOT NULL,
   `quantity` int NOT NULL,
-  `price` decimal(10,2) NOT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`OrderID`),
   KEY `Pid` (`Pid`),
   CONSTRAINT `order_ibfk_1` FOREIGN KEY (`Pid`) REFERENCES `product` (`Pid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `cart` (
   `UserId` int NOT NULL,
   `OrderId` int NOT NULL,
@@ -68,3 +66,15 @@ CREATE TABLE `cart` (
   CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`OrderId`) REFERENCES `order` (`OrderID`),
   CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `personalinfo` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DELIMITER //
+CREATE TRIGGER update_order_price BEFORE INSERT ON `order`
+FOR EACH ROW
+BEGIN
+    DECLARE product_price DECIMAL(10, 2);
+    SELECT price INTO product_price FROM product WHERE Pid = NEW.Pid;
+    SET NEW.price = NEW.quantity * product_price;
+END;
+//
+DELIMITER ;
+
